3019993f3ebfbe5a74f657b7582d44a721d087a0
knyshv <knyshv2412@gmail.com>
2016-11-20 22:12:13 +0200

    initial commit, collected some previous code

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+//function initMap() {
+//	var mapDiv = document.getElementById('map');
+//	var map = new google.maps.Map(mapDiv, {
+//		center: {lat: 48.2957486, lng: 26.6975965}, //48.2957486,26.6975965 ua coords
+//		zoom: 7
+//	});
+//	}
+//
+//
+//$(document).ready(function(){
+//
+//});
